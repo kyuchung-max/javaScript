ae75b44e12700331ac39b389074381d02d2dfc72
kyuchung-max <kyuchung@daum.net>
2021-06-11 13:20:41 +0900

    js file add

---
+++ seats/seat.js
@@ -0,0 +1,13 @@
+
+        var customer = prompt("입장객 수?");
+        var colNum = prompt("한 줄에 몇명?");
+        var rowNum;
+        if(customer % colNum === 0){
+            rowNum = parseInt(customer/colNum);
+            else
+                rowNum =
+        }
+         // customer/colNum;
+        document.write(rowNum + "개의 줄이 필요");
+        
+    
