71fa57de2b6fa7b6b939b96da41e35329cc5bfa8
kyuchung-max <kyuchung@daum.net>
2021-06-17 10:05:44 +0900

    js add

--- js/event.js
+++ js/event.js
@@ -1,9 +1,15 @@
 function showText(){
-    var text= document.getElementById("desc");
-    text.style.display="block";
+   // var text= document.getElementById("desc");
+    //text.style.display="block";
+    document.getElementById("desc").style.display="block";
+    var button = document.getElementById("btn");
+    button.style.display = "none";
 }
 
 function hideText(){
     var text= document.getElementById("desc");
     text.style.display="none";
+
+    var button = document.getElementById("btn");
+    button.style.display = "none";
 }
